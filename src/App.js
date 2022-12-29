fa3cc9ab1cc493862af51d7db39af143db03e246
Deadpool1 <atwood169@yahoo.com>
2022-12-29 13:28:39 -0600

    updated background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import backgroundImg from './assets/img/back.jpg';
 
 function App() {
   return (
+    <div style={{
+      backgroundImage: `url(${backgroundImg})`
+    }}>
     <div className="App">
       <NavBar />
       <Banner />
+    </div>
     </div>
   );
 }
